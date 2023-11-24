47495edb2c8f204bb31f2c06bbcd5fc29af666d4
Knot <aleknot.696@gmail.com>
2023-11-24 19:54:57 -0300

    avances

---
+++ funcionesCarrito.js
@@ -0,0 +1,10 @@
+/* function agregarAlCarrito(producto) {
+  const memoria = localStorage.getItem('articulos');
+  console.log(memoria);
+  if(!memoria){
+    const nuevoProducto = producto;
+    nuevoProducto.cantidad = 1;
+    localStorage.setItem('articulos',JSON.stringify([nuevoProducto]));
+  }
+
+} */
--- indexAlaskan.js
+++ indexAlaskan.js
@@ -1,21 +1,45 @@
 const contenedorTarjetas = document.querySelector('.main')
 
-function crearArticulos(productos){
-    productos.forEach(producto => {
-        const nuevoArticulo = document.createElement('article');
-        nuevoArticulo.innerHTML = `
+function crearArticulos(productos) {
+  productos.forEach(producto => {
+    const nuevoArticulo = document.createElement('article');
+    nuevoArticulo.innerHTML = `
             <img src=${producto.imagen}>
-            <h3>${producto.nombre}</h3>
+            <h2>${producto.nombre}</h2>
             <ul>
                 <li>${producto.descripcion1}</li>
                 <li>${producto.descripcion2}</li>
                 <li>${producto.descripcion3}</li>
             </ul>
-            <h3>Valor: $${producto.precio}</h3>
-            <button id='botonAgregar'>Agregar al carrito</button>
+            <h3>Valor: $</h3>
+            <h3>${producto.precio}</h3>
+            <button class='botonAgregar'>Agregar al carrito</button>
         `
-        contenedorTarjetas.appendChild(nuevoArticulo);
-    });
+    contenedorTarjetas.appendChild(nuevoArticulo);
+
+    let productosCarrito=[];
+
+    nuevoArticulo.addEventListener('click',e =>{
+      
+      if(e.target.classList.contains('botonAgregar')){
+        const producto = e.target.parentElement
+
+        const infoProducto={
+          cantidad:1,
+          nombre: producto.querySelector('h2').textContent,
+          valor: producto.querySelectorAll('h3')[1].textContent,
+        }
+
+        productosCarrito= [...productosCarrito, infoProducto]
+
+        
+        
+      }
+      console.log(productosCarrito)
+    })
+    
+    /* nuevoArticulo.getElementsByTagName('button')[0].addEventListener('click', () => agregarAlCarrito(producto)) */
+  });
 }
 
 crearArticulos(articulosAlaskan);
